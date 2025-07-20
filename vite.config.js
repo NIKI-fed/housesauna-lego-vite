e9836bee49545e124e48b563dcd5ffcf16551f10
NIKI-fed <nikifedorov@mail.ru>
2025-07-21 02:11:52 +0300

    Сборка проекта на Vite/Настройка конфигурационного файла, замена импортов на @use, замена ссылок на картинки

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,15 @@
 import { defineConfig } from 'vite';
-// import vueSvgPlugin from '@vitejs/plugin-vue-svg'; // для обработки SVG
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer'
 import { resolve } from 'path';
-import cssnano from 'cssnano';
+import path from 'path';
 import autoprefixer from 'autoprefixer';
+import cssnano from 'cssnano';
 import postcssUrl from 'postcss-url';
 
-// const root = resolve(__dirname, '.')
 const root = resolve(__dirname, './src')
 const outDir = resolve(__dirname, 'dist')
 
 export default defineConfig({
-
         preview: {
         port: 8080,
         strictPort: true,
@@ -26,7 +24,6 @@ export default defineConfig({
 
     plugins: [
                 ViteImageOptimizer({
-                    // https://github.com/FatehAK/vite-plugin-image-optimizer
                     png: {
                         quality: 70,
                     },
@@ -41,20 +38,45 @@ export default defineConfig({
 
     root,
 
-        resolve: {
-            alias: {
-                // '@': resolve(__dirname, '/node_modules/bootstrap'),
-                // 'bootstrap': resolve(__dirname, '/node_modules/bootstrap'),
-            }
+    resolve: {
+        alias: {
+            '@img': path.resolve(root, 'img'),
+            '@': root
+        }
+    },
+
+    css: {
+        devSourcemap: true,    // Включает source maps в режиме разработки
+        sourcemap: true,       // Включает source maps для production-сборки
+        postcss: {
+            map: true,
+            plugins: [
+                // require('postcss-discard-comments')(),
+                // require('autoprefixer')({
+                //     map: true
+                // }),
+                autoprefixer(),
+                cssnano({ preset: 'default' }),
+                postcssUrl({ url: 'rebase' })
+            ],
         },
+        preprocessorOptions: {
+            scss: {
+                sourceMap: true,  // Включение source maps для SCSS
+                // implementation: 'sass',
+                // additionalData: `@use "@/styles/variables.scss" as *;`
+            },
+        },
+    },
 
     build: {
         outDir,   // Директория вывода результатов сборки
+        sourcemap: true,  // Генерация Source Maps для JS и CSS
         emptyOutDir: true,
         assetsInlineLimit: 4096, // Максимальный размер ресурсов для встраивания inline
-        minify: true,     // Минификация JS и HTML файлов
-        sourcemap: true,  // Генерация Source Maps для JS и CSS
-        
+        minify: 'esbuild',     // Минификация JS и HTML файлов
+        cssMinify: 'esbuild', // Минификация с сохранением source maps
+
         rollupOptions: {
             input: {
                 main: resolve(root, 'index.html'),
@@ -68,81 +90,27 @@ export default defineConfig({
             },
 
             output: {
-                // entryFileNames: '[name].js',
-                // chunkFileNames: '[name]-[hash].js',                
+                entryFileNames: 'js/[name].js',
+                chunkFileNames: 'js/[name]-[hash].js',                
                 assetFileNames: ({ name }) => {
-                                    name = name.toLowerCase()
+                                    name = name?.toLowerCase() ?? '';
 
-                                    if (/\\.(ttf|otf|woff|woff2|eot)$/.test(name)) {
+                                    if (/\.(ttf|otf|woff|woff2|eot)$/.test(name)) {
                                         return 'fonts/[name]-[hash][extname]'
                                     }
                 
-                                    if (/\.(gif|jpe?g|png|svg)$/.test(name)){
+                                    if (/\.(gif|jpe?g|png|svg|webp)$/.test(name)){
                                         return 'img/[name]-[hash][extname]'
                                     }
                 
                                     if (/\.css$/.test(name)) {
                                         return 'css/style-[hash][extname]'
                                     }
-                
-                                    // default value
+
                                     return 'assets/[name]-[hash][extname]'
                                 },
             },
         },
     },
-
-    // css: {
-    //     postcss: {
-    //         plugins: [
-    //             autoprefixer(),
-    //             cssnano({ preset: ['default'] }), // Оптимизация и минимизация CSS
-    //             postcssUrl({
-    //                 url: 'inline', // Inline for small images or fonts
-    //             }),
-    //         ],
-    //     },
-    // },
-
+    
 });
-
-
-
-
-
-// export default defineConfig({
-
-
-//     build: {
-//         outDir,
-//         emptyOutDir: true,
-//         rollupOptions: {
-//             input: {
-//                 main: resolve(root, 'index.html'),
-//                 about: resolve(root, 'about.html'),
-//             },
-
-//             // output: { // remove hashes from filenames on build
-//             //     entryFileNames: `assets/[name].js`,
-//             //     chunkFileNames: `assets/[name].js`,
-//             //     assetFileNames: `assets/[name].[ext]`,
-//             // },
-//             output: {
-//                 assetFileNames: ({ name }) => {
-//                     name = name.toLowerCase()
-
-//                     if (/\.(gif|jpe?g|png|svg)$/.test(name ?? '')){
-//                         return 'assets/images/[name]-[hash][extname]'
-//                     }
-
-//                     if (/\.css$/.test(name ?? '')) {
-//                         return 'assets/styles/[name]-[hash][extname]'
-//                     }
-
-//                     // default value
-//                     return 'assets/[name]-[hash][extname]'
-//                 },
-//             },
-//         }
-//     },
-// })
